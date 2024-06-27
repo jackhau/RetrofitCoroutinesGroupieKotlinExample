6d8e5678f59fb7b4757b7f1f583d64617372ab96
jack hau tan <jackhau@agmostudio.com>
2024-06-27 15:13:32 +0800

    integrate flipper network interceptor

---
+++ app/src/main/java/com/example/retrofitkolinexample/MyApplication.kt
@@ -0,0 +1,10 @@
+package com.example.retrofitkolinexample
+
+import android.app.Application
+
+open class MyApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
